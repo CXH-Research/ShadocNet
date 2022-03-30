042ff0b888ee8b67f501dbf226957e1fec7b6651
xuhangc <cxhhg11@gmail.com>
2022-03-30 17:06:27 +0800

    test

--- test.py
+++ test.py
@@ -10,15 +10,15 @@ import torch.nn.functional as F
 import utils
 
 from data import get_test_data
-
+from model import Model
 from skimage import img_as_ubyte
 from pdb import set_trace as stx
 
-parser = argparse.ArgumentParser(description='Image Deraining using MPRNet')
+parser = argparse.ArgumentParser(description='Shadow Removal')
 
-parser.add_argument('--input_dir', default='./Datasets/test/', type=str, help='Directory of validation images')
+parser.add_argument('--input_dir', default='./dataset/test/', type=str, help='Directory of validation images')
 parser.add_argument('--result_dir', default='./results/', type=str, help='Directory for results')
-parser.add_argument('--weights', default='./pretrained_models/model_deraining.pth', type=str, help='Path to weights')
+parser.add_argument('--weights', default='./pretrained_models/model_best.pth', type=str, help='Path to weights')
 parser.add_argument('--gpus', default='0', type=str, help='CUDA_VISIBLE_DEVICES')
 
 args = parser.parse_args()
@@ -26,3 +26,37 @@ args = parser.parse_args()
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus
 
+model = Model()
+utils.load_checkpoint(model, args.weights)
+print("===>Testing using weights: ", args.weights)
+model.cuda()
+model_restoration = nn.DataParallel(model)
+model_restoration.eval()
+
+datasets = ['ISTD', 'SRD']
+
+for dataset in datasets:
+    dir_test = os.path.join(args.input_dir, dataset, 'input')
+    test_dataset = get_test_data(dir_test, img_options={})
+    test_loader = DataLoader(dataset=test_dataset, batch_size=1, shuffle=False, num_workers=16, drop_last=False,
+                             pin_memory=True)
+
+    result_dir = os.path.join(args.result_dir, dataset)
+    utils.mkdir(result_dir)
+
+    with torch.no_grad():
+        for ii, data_test in enumerate(tqdm(test_loader), 0):
+            torch.cuda.ipc_collect()
+            torch.cuda.empty_cache()
+
+            input_ = data_test[0].cuda()
+            filenames = data_test[1]
+
+            restored = model_restoration(input_)
+            restored = torch.clamp(restored[0], 0, 1)
+
+            restored = restored.permute(0, 2, 3, 1).cpu().detach().numpy()
+
+            for batch in range(len(restored)):
+                restored_img = img_as_ubyte(restored[batch])
+                utils.save_img((os.path.join(result_dir, filenames[batch] + '.png')), restored_img)
